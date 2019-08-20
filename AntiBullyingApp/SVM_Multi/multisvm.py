e8eff5dcb83d2e47f9bdb73bb4c912bf78d218ec
husenbhagat <49280980+husenbhagat@users.noreply.github.com>
2019-08-20 15:38:22 -0400

    Update multisvm.py

--- AntiBullyingApp/SVM_Multi/multisvm.py
+++ AntiBullyingApp/SVM_Multi/multisvm.py
@@ -15,11 +15,11 @@ from sklearn.svm import LinearSVC
 
 clf = sklearn.svm.LinearSVC()
 
-training_files = sklearn.datasets.load_files("/home/ubuntu/Desktop/SVM_Multi/dataset_training")
+training_files = sklearn.datasets.load_files("/../../../SVM_Multi/dataset_training")
 
 	#print training_files.data
 
-predict_files = sklearn.datasets.load_files("/home/ubuntu/Desktop/SVM/dataset_prediction")
+predict_files = sklearn.datasets.load_files("/../../../SVM/dataset_prediction")
 
 print "Predict",predict_files.data
 
@@ -36,14 +36,14 @@ X_p = vectorizer.transform((open(f).read() for f in predict_files.filenames))
 y= OneVsOneClassifier(LinearSVC(random_state=0)).fit(X_t,training_files.target).predict(X_p)	
 print y[0]
 if y[0]==0:
-	f1=open("/home/ubuntu/Desktop/SVM/out.txt",'w')
+	f1=open("/../../../SVM/out.txt",'w')
 	f1.write("0")
 	f1.close()
 elif y[0]==1:
-	f1=open("/home/ubuntu/Desktop/SVM/out.txt",'w')
+	f1=open("/../../../SVM/out.txt",'w')
 	f1.write("1")
 	f1.close()
 elif y[0]==2:
-	f1=open("/home/ubuntu/Desktop/SVM/out.txt",'w')
+	f1=open("/../../../SVM/out.txt",'w')
 	f1.write("2")
 	f1.close()
